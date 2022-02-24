1d1effe0e0ecb2c09f67e65aaa4947d9f1558ecc
Andrea Castillo <castiyeauxdev@gmail.com>
2022-02-23 19:26:17 -0600

    added lurk pic and slide in

--- Day3-RotatingNavigation/script.js
+++ Day3-RotatingNavigation/script.js
@@ -1,8 +1,11 @@
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
+const lurkbuttz = document.querySelector('.lurkbuttz');
 
 open.addEventListener('click', () => container.classList.add('show-nav'));
+open.addEventListener('click', () => lurkbuttz.classList.add('show-nav'));
 
 close.addEventListener('click', () => container.classList.remove('show-nav'));
+close.addEventListener('click', () => lurkbuttz.classList.remove('show-nav'));
 
